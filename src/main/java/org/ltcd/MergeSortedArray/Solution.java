96cb8d81600b545dcc3ec9d55d286dc4fc9b873b
JBRKR000 <kozimorjakub@gmail.com>
2025-01-16 17:01:31 +0100

    Created project for solving LeetCode problems

---
+++ src/main/java/org/ltcd/MergeSortedArray/Solution.java
@@ -0,0 +1,11 @@
+package org.ltcd.MergeSortedArray;
+
+class Solution {
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
