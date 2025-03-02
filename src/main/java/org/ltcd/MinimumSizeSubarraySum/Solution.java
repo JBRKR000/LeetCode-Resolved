fc958652924c9574cbc0843ed3f35286d7b24696
JBRKR000 <kozimorjakub@gmail.com>
2025-03-02 15:39:04 +0100

    Added solution for MinimumSizeSubarraySum

---
+++ src/main/java/org/ltcd/MinimumSizeSubarraySum/Solution.java
@@ -0,0 +1,45 @@
+package org.ltcd.MinimumSizeSubarraySum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+Given an array of positive integers nums and a positive integer target,
+return the minimal length of a subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+Example 1:
+
+Input: target = 7, nums = [2,3,1,2,4,3]
+Output: 2
+Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+Example 2:
+
+Input: target = 4, nums = [1,4,4]
+Output: 1
+Example 3:
+
+Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+Output: 0
+ */
+public class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int n = nums.length;
+        int minLength = Integer.MAX_VALUE;
+        int left = 0;
+        int sum = 0;
+        for (int right = 0; right < n; right++) {
+            sum += nums[right];
+
+            while (sum >= target) {
+                minLength = Math.min(minLength, right - left + 1);
+                sum -= nums[left];
+                left++;
+            }
+        }
+        return minLength == Integer.MAX_VALUE ? 0 : minLength;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().minSubArrayLen(7, new int[]{2,3,1,2,4,3}));
+    }
+}
